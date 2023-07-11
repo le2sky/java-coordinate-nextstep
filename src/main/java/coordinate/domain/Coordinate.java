c11f8f95a9f5e3cde2fc4c2e33376a41a9e1e285
le2sky <leehaneul990623@gmail.com>
2023-07-11 22:42:37 +0900

    fix : 누락된 final 키워드 추가

--- src/main/java/coordinate/domain/Coordinate.java
+++ src/main/java/coordinate/domain/Coordinate.java
@@ -43,7 +43,7 @@ public class Coordinate {
         return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
     }
 
-    private static void checkUnknownCoordinate(Coordinate other) {
+    private static void checkUnknownCoordinate(final Coordinate other) {
         if (other == null) {
             throw new IllegalArgumentException("알 수 없는 값과 거리를 계산할 수 없습니다.");
         }
