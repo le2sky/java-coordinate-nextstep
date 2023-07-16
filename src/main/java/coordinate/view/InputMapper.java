3c5f93ec32eecdd9b365cc0bce80816ba407f0c3
le2sky <leehaneul990623@gmail.com>
2023-07-16 16:33:10 +0900

    style : 가로 길이 초과된 상수 다시 포매팅

--- src/main/java/coordinate/view/InputMapper.java
+++ src/main/java/coordinate/view/InputMapper.java
@@ -10,7 +10,8 @@ public class InputMapper {
 
     private static final Pattern COORDINATE_NUMBER_PATTERN = Pattern.compile("\\d+");
     private static final Pattern COORDINATE_PATTERN = Pattern.compile("\\(\\d+,\\d+\\)");
-    private static final String INVALID_COORDINATE_FORMAT_MESSAGE = "좌표의 형식은 (x,y) 입니다. 여러 좌표를 입력하시려면 - 를 구분자로 이용해주세요.";
+    private static final String INVALID_COORDINATE_FORMAT_MESSAGE
+            = "좌표의 형식은 (x,y) 입니다. 여러 좌표를 입력하시려면 - 를 구분자로 이용해주세요.";
     private static final String SPLIT_DELIMITER = "-";
 
     public static List<Coordinate> mapToCoordinates(final String input) {
