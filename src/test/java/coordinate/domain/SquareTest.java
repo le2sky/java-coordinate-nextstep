bbf7091510ca110ef85b1fb134334c758707d36e
le2sky <leehaneul990623@gmail.com>
2023-07-22 19:59:36 +0900

    refactor : 테스트 코드를 각 도형 객체 테스트 내부로 이동

--- src/test/java/coordinate/domain/SquareTest.java
+++ src/test/java/coordinate/domain/SquareTest.java
@@ -9,47 +9,18 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class FigureFactoryTest {
-
-    @DisplayName("두 좌표를 받으면, 직선을 생성한다.")
-    @Test
-    void calculateDistance() {
-        FigureFactory coordinatecalculator = new FigureFactory();
-        Point from = Point.of(10, 10);
-        Point into = Point.of(14, 15);
-
-        Straight result = coordinatecalculator.makeStraight(from, into);
-
-        assertThat(result.calculateLength()).isEqualTo(6.403124, offset(0.00000099));
-    }
-
-    @DisplayName("주어진 좌표는 존재하는 좌표여야 한다.")
-    @Test
-    void checkPoints() {
-        FigureFactory coordinatecalculator = new FigureFactory();
-        Point from = Point.of(10, 10);
-        Point into = Point.of(14, 15);
-
-        assertThatThrownBy(() -> coordinatecalculator.makeStraight(from, null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("존재하는 좌표값을 입력해주세요.");
-
-        assertThatThrownBy(() -> coordinatecalculator.makeStraight(null, into))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("존재하는 좌표값을 입력해주세요.");
-    }
+class SquareTest {
 
     @DisplayName("좌표의 목록을 받으면 사각형을 생성한다.")
     @Test
     void makeSquare() {
-        FigureFactory coordinatecalculator = new FigureFactory();
         List<Point> points = new ArrayList<>();
         points.add(Point.of(10, 10));
         points.add(Point.of(22, 10));
         points.add(Point.of(22, 18));
         points.add(Point.of(10, 18));
 
-        Square result = coordinatecalculator.makeSquare(points);
+        Square result = Square.from(points);
 
         assertThat(result.calculateSquareArea()).isEqualTo(96.0, offset(0.99));
     }
@@ -57,19 +28,18 @@ class FigureFactoryTest {
     @DisplayName("사각형을 만들기 위해서는 4개의 좌표가 필요하다.")
     @Test
     void checkPointsForMakeSquare() {
-        FigureFactory coordinatecalculator = new FigureFactory();
         List<Point> points = new ArrayList<>();
         points.add(Point.of(10, 10));
         points.add(Point.of(22, 10));
         points.add(Point.of(22, 18));
 
-        assertThatThrownBy(() -> coordinatecalculator.makeSquare(points))
+        assertThatThrownBy(() -> Square.from(points))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형을 만들려면 정확히 4개의 좌표가 필요합니다.");
 
         points.add(Point.of(1, 2));
         points.add(Point.of(3, 13));
-        assertThatThrownBy(() -> coordinatecalculator.makeSquare(points))
+        assertThatThrownBy(() -> Square.from(points))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형을 만들려면 정확히 4개의 좌표가 필요합니다.");
     }
@@ -77,14 +47,13 @@ class FigureFactoryTest {
     @DisplayName("찌그러진 사각형은 허용하지 않는다.")
     @Test
     void checkSquare() {
-        FigureFactory coordinatecalculator = new FigureFactory();
         List<Point> points = new ArrayList<>();
         points.add(Point.of(8, 4));
         points.add(Point.of(10, 3));
         points.add(Point.of(6, 1));
         points.add(Point.of(11, 1));
 
-        assertThatThrownBy(() -> coordinatecalculator.makeSquare(points))
+        assertThatThrownBy(() -> Square.from(points))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형은 사다리꼴, 마름모를 제외한 직사각형만 허용합니다.");
     }
@@ -92,14 +61,13 @@ class FigureFactoryTest {
     @DisplayName("사다리꼴은 허용되지 않는다.")
     @Test
     void checkTrapezoid() {
-        FigureFactory coordinatecalculator = new FigureFactory();
         List<Point> points = new ArrayList<>();
         points.add(Point.of(8, 4));
         points.add(Point.of(11, 4));
         points.add(Point.of(6, 1));
         points.add(Point.of(11, 1));
 
-        assertThatThrownBy(() -> coordinatecalculator.makeSquare(points))
+        assertThatThrownBy(() -> Square.from(points))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형은 사다리꼴, 마름모를 제외한 직사각형만 허용합니다.");
     }
@@ -107,14 +75,13 @@ class FigureFactoryTest {
     @DisplayName("등변 사다리꼴은 허용되지 않는다.")
     @Test
     void checkIsoscelesTrapezoid() {
-        FigureFactory coordinatecalculator = new FigureFactory();
         List<Point> points = new ArrayList<>();
         points.add(Point.of(8, 4));
         points.add(Point.of(10, 4));
         points.add(Point.of(6, 1));
         points.add(Point.of(12, 1));
 
-        assertThatThrownBy(() -> coordinatecalculator.makeSquare(points))
+        assertThatThrownBy(() -> Square.from(points))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형은 사다리꼴, 마름모를 제외한 직사각형만 허용합니다.");
     }
@@ -122,14 +89,13 @@ class FigureFactoryTest {
     @DisplayName("마름모는 허용되지 않는다.")
     @Test
     void checkDiamond() {
-        FigureFactory coordinatecalculator = new FigureFactory();
         List<Point> points = new ArrayList<>();
         points.add(Point.of(8, 3));
         points.add(Point.of(4, 2));
         points.add(Point.of(8, 1));
         points.add(Point.of(12, 2));
 
-        assertThatThrownBy(() -> coordinatecalculator.makeSquare(points))
+        assertThatThrownBy(() -> Square.from(points))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형은 사다리꼴, 마름모를 제외한 직사각형만 허용합니다.");
     }
---
+++ src/test/java/coordinate/domain/StraightTest.java
@@ -0,0 +1,37 @@
+package coordinate.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.offset;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StraightTest {
+
+    @DisplayName("두 좌표를 받으면, 직선을 생성한다.")
+    @Test
+    void calculateDistance() {
+        Point from = Point.of(10, 10);
+        Point into = Point.of(14, 15);
+
+        Straight result = Straight.of(from, into);
+
+        assertThat(result.calculateLength()).isEqualTo(6.403124, offset(0.00000099));
+    }
+
+    @DisplayName("주어진 좌표는 존재하는 좌표여야 한다.")
+    @Test
+    void checkPoints() {
+        Point from = Point.of(10, 10);
+        Point into = Point.of(14, 15);
+
+        assertThatThrownBy(() -> Straight.of(from, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하는 좌표값을 입력해주세요.");
+
+        assertThatThrownBy(() -> Straight.of(null, into))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하는 좌표값을 입력해주세요.");
+    }
+}
