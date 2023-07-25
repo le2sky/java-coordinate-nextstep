3a4d99a899ae79da4fbcd0ad616290828dfff826
le2sky <leehaneul990623@gmail.com>
2023-07-26 06:31:17 +0900

    fix : 중복 테스트 제거

--- src/test/java/coordinate/domain/SquareTest.java
+++ src/test/java/coordinate/domain/SquareTest.java
@@ -99,26 +99,4 @@ class SquareTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사각형은 사다리꼴, 마름모를 제외한 직사각형만 허용합니다.");
     }
-
-    @DisplayName("주어진 좌표는 모두 존재하는 좌표여야 한다.")
-    @Test
-    void checkPointsNull() {
-        List<Point> points = new ArrayList<>();
-        points.add(Point.of(8, 3));
-        points.add(Point.of(4, 2));
-        points.add(null);
-        points.add(Point.of(12, 2));
-
-        assertThatThrownBy(() -> Square.from(points))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("존재하는 좌표값을 입력해주세요.");
-    }
-
-    @DisplayName("주어진 좌표 목록 자체는 널값이 될 수 없다.")
-    @Test
-    void checkNull() {
-        assertThatThrownBy(() -> Square.from(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표 목록을 입력해주세요.");
-    }
 }
