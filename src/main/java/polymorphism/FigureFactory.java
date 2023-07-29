73581480b4d8a8ebe668d79bf443f5df3a04298c
le2sky <leehaneul990623@gmail.com>
2023-07-29 23:53:28 +0900

    refactor : map을 이용해서 getInstance 개선

--- src/main/java/polymorphism/FigureFactory.java
+++ src/main/java/polymorphism/FigureFactory.java
@@ -1,22 +1,28 @@
 package polymorphism;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class FigureFactory {
 
+    private static final Map<Integer, Function<List<Point>, Figure>> classifier;
+
+    static {
+        classifier = new HashMap<>();
+        classifier.put(Line.LINE_POINT_SIZE, Line::new);
+        classifier.put(Triangle.TRIANGLE_POINT_SIZE, Triangle::new);
+        classifier.put(Rectangle.RECTANGLE_POINT_SIZE, Rectangle::new);
+    }
+
     static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
+        Function<List<Point>, Figure> figureConstructor = classifier.get(points.size());
+
+        if (figureConstructor == null) {
+            throw new IllegalArgumentException("유효하지 않은 도형입니다.");
         }
 
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        return figureConstructor.apply(points);
     }
 }
