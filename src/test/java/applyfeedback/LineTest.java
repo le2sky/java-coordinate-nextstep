faecc022e1b9e79aca46c6483343c469a27b1b97
le2sky <leehaneul990623@gmail.com>
2023-08-01 23:07:21 +0900

    refactor : 중복 테스트 코드 통합

--- src/test/java/applyfeedback/LineTest.java
+++ src/test/java/applyfeedback/LineTest.java
@@ -11,39 +11,6 @@ import org.junit.jupiter.api.Test;
 
 class LineTest {
 
-    @DisplayName("좌표가 널값인 경우 예외를 발생함")
-    @Test
-    void checkPointsNull() {
-        assertThatThrownBy(() -> Line.from(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표를 입력해주세요.");
-    }
-
-    @DisplayName("좌표 목록에 널이 포함되어 있는 경우, 예외를 발생함")
-    @Test
-    void hasNull() {
-        List<Point> points = new ArrayList<>();
-        points.add(null);
-        points.add(Point.of(1, 2));
-
-        assertThatThrownBy(() -> Line.from(points))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표를 입력해주세요.");
-    }
-
-    @DisplayName("좌표 목록에 중복 좌표가 포함되어 있는 경우, 예외를 발생함")
-    @Test
-    void hasDuplicatedPoint() {
-        List<Point> points = new ArrayList<>();
-        points.add(Point.of(1, 2));
-        points.add(Point.of(1, 2));
-
-        assertThatThrownBy(() -> Line.from(points))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표를 입력해주세요.");
-    }
-
-
     @DisplayName("직선의 좌표는 두개이다. 두개가 아닌 경우, 예외를 발생함")
     @Test
     void checkPointsSizeOfLine() {
--- src/test/java/applyfeedback/RectangleTest.java
+++ src/test/java/applyfeedback/RectangleTest.java
@@ -1,43 +1,5 @@
 package applyfeedback;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 class RectangleTest {
 
-    @DisplayName("좌표가 널값인 경우 예외를 발생함")
-    @Test
-    void checkPointsNull() {
-        assertThatThrownBy(() -> Rectangle.from(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표를 입력해주세요.");
-    }
-
-    @DisplayName("좌표 목록에 널이 포함되어 있는 경우, 예외를 발생함")
-    @Test
-    void hasNull() {
-        List<Point> points = new ArrayList<>();
-        points.add(null);
-        points.add(Point.of(1, 2));
-
-        assertThatThrownBy(() -> Rectangle.from(points))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표를 입력해주세요.");
-    }
-
-    @DisplayName("좌표 목록에 중복 좌표가 포함되어 있는 경우, 예외를 발생함")
-    @Test
-    void hasDuplicatedPoint() {
-        List<Point> points = new ArrayList<>();
-        points.add(Point.of(1, 2));
-        points.add(Point.of(1, 2));
-
-        assertThatThrownBy(() -> Rectangle.from(points))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("유효한 좌표를 입력해주세요.");
-    }
 }
