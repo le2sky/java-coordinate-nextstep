0e858c142a80fe9a1d143fc384a42488770d6e6c
le2sky <leehaneul990623@gmail.com>
2023-08-08 08:39:55 +0900

    feat : FigureFactory 구현

--- src/main/java/applyfeedback/FigureFactory.java
+++ src/main/java/applyfeedback/FigureFactory.java
@@ -1,5 +1,21 @@
 package applyfeedback;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 public class FigureFactory {
 
+    private static final Map<Integer, Function<List<Point>, Figure>> classifier = new HashMap<>();
+
+    static {
+        classifier.put(2, Line::from);
+        classifier.put(3, Triangle::from);
+        classifier.put(4, Rectangle::from);
+    }
+
+    public static Figure getInstance(final List<Point> points) {
+        return classifier.get(points.size()).apply(points);
+    }
 }
---
+++ src/test/java/applyfeedback/FigureFactoryTest.java
@@ -0,0 +1,43 @@
+package applyfeedback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class FigureFactoryTest {
+
+    @Test
+    public void line() {
+        List<Point> points = Arrays.asList(
+                Point.of(1, 2),
+                Point.of(2, 3));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOfAny(Line.class);
+    }
+
+    @Test
+    public void triangle() {
+        List<Point> points = Arrays.asList(
+                Point.of(1, 1),
+                Point.of(4, 1),
+                Point.of(1, 4));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOfAny(Triangle.class);
+    }
+
+    @Test
+    public void rectangle() {
+        List<Point> points = Arrays.asList(
+                Point.of(1, 1),
+                Point.of(4, 1),
+                Point.of(1, 4),
+                Point.of(4, 4));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOfAny(Rectangle.class);
+    }
+}
