ab9bb27e867e26cd0efc59126ccde95a996bb84f
le2sky <leehaneul990623@gmail.com>
2023-08-08 08:50:04 +0900

    feat : 좌표 목록 크기 검증 기능 구현

--- src/main/java/applyfeedback/FigureFactory.java
+++ src/main/java/applyfeedback/FigureFactory.java
@@ -11,6 +11,8 @@ public class FigureFactory {
     private static final int SIZE_OF_LINE = 2;
     private static final int SIZE_OF_TRIANGLE = 3;
     private static final int SIZE_OF_RECTANGLE = 4;
+    private static final String INVALID_POINT_SIZE_FOR_CLASSIFICATION =
+            "해당 좌표 목록으로 만들 수 있는 도형은 존재하지 않습니다.";
 
     static {
         classifier.put(SIZE_OF_LINE, Line::from);
@@ -19,6 +21,16 @@ public class FigureFactory {
     }
 
     public static Figure getInstance(final List<Point> points) {
-        return classifier.get(points.size()).apply(points);
+        Function<List<Point>, Figure> figureStaticFactoryMethod = classifier.get(points.size());
+        checkPointsSizeForClassification(figureStaticFactoryMethod);
+
+        return figureStaticFactoryMethod.apply(points);
+    }
+
+    private static void checkPointsSizeForClassification(
+            final Function<List<Point>, Figure> figureStaticFactoryMethod) {
+        if (figureStaticFactoryMethod == null) {
+            throw new IllegalArgumentException(INVALID_POINT_SIZE_FOR_CLASSIFICATION);
+        }
     }
 }
--- src/test/java/applyfeedback/FigureFactoryTest.java
+++ src/test/java/applyfeedback/FigureFactoryTest.java
@@ -1,13 +1,17 @@
 package applyfeedback;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FigureFactoryTest {
 
+    @DisplayName("좌표가 2개 주어지면 직선을 반환한다.")
     @Test
     public void line() {
         List<Point> points = Arrays.asList(
@@ -18,6 +22,7 @@ class FigureFactoryTest {
         assertThat(figure).isInstanceOfAny(Line.class);
     }
 
+    @DisplayName("좌표가 3개 주어지면 삼각형을 반환한다.")
     @Test
     public void triangle() {
         List<Point> points = Arrays.asList(
@@ -29,6 +34,7 @@ class FigureFactoryTest {
         assertThat(figure).isInstanceOfAny(Triangle.class);
     }
 
+    @DisplayName("좌표가 4개 주어지면 사각형을 반환한다.")
     @Test
     public void rectangle() {
         List<Point> points = Arrays.asList(
@@ -39,5 +45,20 @@ class FigureFactoryTest {
 
         Figure figure = FigureFactory.getInstance(points);
         assertThat(figure).isInstanceOfAny(Rectangle.class);
+    }
+
+    @DisplayName("정해진 도형 이외에는 반환할 수 없다. 정해진 도형은 직선, 삼각형, 사각형을 의미한다.")
+    @Test
+    void checkPointsLength() {
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(Point.of(1, 2));
+        points.add(Point.of(1, 2));
+        points.add(Point.of(1, 2));
+        points.add(Point.of(1, 2));
+        points.add(Point.of(1, 2));
+
+        assertThatThrownBy(() -> FigureFactory.getInstance(points))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 좌표 목록으로 만들 수 있는 도형은 존재하지 않습니다.");
     }
 }
