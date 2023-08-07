a52d48feb2c055a34d76985370c402ca20a5bbc6
le2sky <leehaneul990623@gmail.com>
2023-08-08 08:41:32 +0900

    refactor : 하드 코딩 값 상수화

--- src/main/java/applyfeedback/FigureFactory.java
+++ src/main/java/applyfeedback/FigureFactory.java
@@ -8,11 +8,14 @@ import java.util.function.Function;
 public class FigureFactory {
 
     private static final Map<Integer, Function<List<Point>, Figure>> classifier = new HashMap<>();
+    private static final int SIZE_OF_LINE = 2;
+    private static final int SIZE_OF_TRIANGLE = 3;
+    private static final int SIZE_OF_RECTANGLE = 4;
 
     static {
-        classifier.put(2, Line::from);
-        classifier.put(3, Triangle::from);
-        classifier.put(4, Rectangle::from);
+        classifier.put(SIZE_OF_LINE, Line::from);
+        classifier.put(SIZE_OF_TRIANGLE, Triangle::from);
+        classifier.put(SIZE_OF_RECTANGLE, Rectangle::from);
     }
 
     public static Figure getInstance(final List<Point> points) {
